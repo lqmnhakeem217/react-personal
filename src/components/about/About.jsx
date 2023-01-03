08ef1dd4c5e466c2cbfbed37590d76497ee9b8c0
lqmnhakeem217 <luqhakeem7@gmail.com>
2023-01-03 20:44:02 +0800

    About Section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
 import './about.css'
-
+import ME from '../../assets/me-about.jfif'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>+2 Years Working</small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>200+ WorldWide</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className='about__icon' />
+              <h5>Project</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+          <p>
+            An Inspired Web Developer Seeking for Opportunities in the IT
+            and Software Development. Conducted Few Companies Projects and
+            Some Personal Projects for Personal Enjoyment. Learnt Most of
+            Them In Youtube, StackOverflow and Sample Project Found On the
+            Internet. Good in Team-Building Projects, Creative Problem-Solver
+            and Excellent on Receiving Critism.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
